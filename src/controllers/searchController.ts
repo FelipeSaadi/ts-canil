9d8c1e47eba20837a7eabb16d529623ab7348666
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-07-20 22:03:13 -0300

    feat: add final project

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from 'express'
+import { Pet } from '../models/pet'
+import { createMenuObject } from '../helpers/createMenuObject'
 
 export const search = (req: Request, res: Response) => {
+  const query: string = req.query.q as string
 
+  if (!query) {
+    res.redirect('/')
+    return
+  }
+
+  const data = Pet.getFromName(query)
+
+  res.render('pages/page', {
+    menu: createMenuObject(''),
+    data,
+    query
+  })
 }
